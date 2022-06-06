abe75cfba47b320b7d4acd0a449a8018671de549
Kauan Decarli <kauandecarli@gmail.com>
2022-06-06 16:00:14 -0300

    top_process.c: add Unicode support

--- utils/blocks/top_process.c
+++ utils/blocks/top_process.c
@@ -18,21 +18,25 @@
 int main()
 {
     float usage;
-    char cmd[256];
+    char cmd[CMDLEN*2];
     char exec[40];
     sprintf(exec, "exec -a custom top -b -d1 -w%d", CMDLEN-1);
     FILE *p = popen(exec, "r");
     while(1)
     {
-        for(int i = 0; i < 9; i++)
+        for(int line = 0; line < 9; line++)
         {
-            fgets(cmd, 256, p);
-            if(i == 7)
+            fgets(cmd, CMDLEN*2, p);
+            if(line == 7)
             {
-                for(int i = CMDLEN-5; i < CMDLEN; i++)
+                int len = strlen(cmd);
+
+                for(int i = len-5; i < len; i++)
                     if(cmd[i] == ','){ cmd[i] = '.'; break; }
-                sscanf(&cmd[CMDLEN-6], "%f", &usage);
-                for(int i = 0; i < CMDLEN; i++)
+
+                sscanf(&cmd[len-6], "%f", &usage);
+
+                for(int i = 0; i < len; i++)
                 {
                     if(cmd[i] == ' ')
                     {
@@ -48,10 +52,12 @@ int main()
                         }
                     }
                 }
-                if(strlen(cmd) > 15)
-                    if(cmd[0] == '/' || cmd[0] == '.' || cmd[1] == ':')
+
+                len = strlen(cmd);
+                if(len > 15)
+                    if(cmd[0] == '/' || cmd[0] == '\\' || cmd[0] == '.' || cmd[1] == ':')
                     {
-                        for(int i = 0; i < CMDLEN; i++)
+                        for(int i = 0;; i++)
                         {
                             if(cmd[i] == 0)
                             {
@@ -70,24 +76,36 @@ int main()
                             }
                         }
                     }
-                char out[25];
-                int len = strlen(cmd);
-                if(len > 16)
-                    cmd[15] = 0;
-                strcpy(out, cmd);
-                for(int i = 0; i < 16-len; i++)
-                    out[i+len] = ' ';
-                int x = 15;
-                if(len > 15)
-                    out[14] = '+';
-                if(usage < 10)
-                    x++;
-                sprintf(&out[x], " %.1f%%", usage);
-                for(int i = 0; i < 21; i++)
-                    if(out[i] == 0) out[i] = ' ';
-                out[21] = '\n';
-                out[22] = 0;
-                fputs(out, stdout);
+                int max_cmd_len_bytes, max_cmd_len_chars;
+                max_cmd_len_bytes = max_cmd_len_chars = usage < 10 ? 16 : 15;
+
+                len = 0;
+                char *s = cmd;
+                while(*s)
+                {
+                    int char_len = 1;
+                    if      ((*s & 0xE0) == 0xC0) char_len = 2;
+                    else if ((*s & 0xF0) == 0xE0) char_len = 3;
+                    else if ((*s & 0xF8) == 0xF0) char_len = 4;
+                    s += char_len;
+                    max_cmd_len_bytes += char_len - 1;
+                    len++;
+                }
+
+                if(len > max_cmd_len_chars)
+                {
+                    cmd[max_cmd_len_bytes-1] = '+';
+                    cmd[max_cmd_len_bytes] = 0;
+                    len = max_cmd_len_chars;
+                }
+
+                printf("%s", cmd);
+
+                for(int i = len; i < max_cmd_len_chars; i++)
+                    printf(" ");
+
+                printf(" %.1f%%\n", usage);
+
                 fflush(stdout);
             }
         }
